217b75a0c8b00c2feab427734a028cd4acc11113
kraine <kraine212@gmail.com>
2022-10-30 15:04:00 +0200

    review slider

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,10 +1,12 @@
 
-import modals from './modules/modal'
+import modals from './modules/modal';
+import sliders from './modules/sliders';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
     modals();
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     
 });
---
+++ Source/src/js/modules/sliders.js
@@ -0,0 +1,46 @@
+const sliders = (slides, dir, prev, next) => {
+    let slideIndex = 1,
+        items = document.querySelectorAll(slides);
+        
+    function showSlides(n) {
+        if (n > items.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = items.length;
+        }
+
+        items.forEach( item => {
+            item.classList.add('animated');
+            item.style.display = 'none';
+        });
+
+        items[slideIndex - 1].style.display = 'block';
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides (n) {
+        showSlides (slideIndex += n);
+    }
+
+    try {
+        const prevBtn = document.querySelector(prev),
+            nextBtn = document.querySelector(next);
+
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
+            
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft');
+        });
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+        });
+    } catch (e){ }
+
+};
+
+export default sliders;
