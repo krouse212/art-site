408f64b94572b9b0bc26ecc1a634d20ed102e8f5
kraine <kraine212@gmail.com>
2022-11-14 11:12:56 +0200

    accordion on css

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -8,6 +8,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -24,4 +25,5 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
     pictureSize('.sizes-block');
+    accordion(' .accordion-heading', '.accordion-block');
 });
